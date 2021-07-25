537dbcae170136f625a957e3a1b5f52be65c26f6
songinforu <147970795@qq.com>
2021-07-25 10:15:04 +0800

    data commit

--- src/components/price/DetailNav.vue
+++ src/components/price/DetailNav.vue
@@ -9,23 +9,218 @@
           </div>
         </div>
         <div class="button-warp">
-          <button>下载电子书</button>
+          <button class="authing-button">下载电子书</button>
+        </div>
+        <div class="authing-title-wrapper">
+          <p>功能比较</p>
+          <div class="table-title">
+            <span> <i class="iconfont icon-kapian"></i>开发者版 </span>
+            <span> <i class="iconfont icon-taiyang_sun61"></i>高级版 </span>
+            <span> <i class="iconfont icon-icon-"></i>企业版 </span>
+          </div>
         </div>
       </div>
     </div>
-    <div class="price-detail"></div>
-    <div class="price-question"></div>
+    <div class="price-detail">
+      <div class="table-content">
+        <div class="authing-table" v-for="(item,index) in list" :key="index">
+          <div class="tr">
+            <span>{{item.title}}</span>
+          </div>
+          <div class="tbody" v-for="(el,elIndex) in item.children" :key="elIndex">
+            <p>{{el.one}}<i></i></p>
+            <p>{{el.two}}</p>
+            <p>{{el.three}}</p>
+            <p>{{el.four}}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+    <QuestionNav />
   </div>
 </template>
 
 <script>
-export default {};
+import QuestionNav from "./QuestionNav"
+export default {
+    components:{
+        QuestionNav,
+    },
+    data(){
+        return{
+            list:[
+                {
+                    title:"认证方式",
+                    children:[
+                        {
+                            one: "外部用户身份",
+                            two: "多达50000个活跃用户",
+                            three: "多达50000个活跃用户",
+                            four: "根据需要"
+                        },
+                        {
+                            one: "社交账号连接",
+                            two: "无限个",
+                            three: "无限个",
+                            four: "无限个"
+                        },
+                        {
+                            one: "内部人员身份",
+                            two: "",
+                            three: "多达 50,000 名员工",
+                            four: "根据需要"
+                        },
+                        {
+                            one: "企业身份连接",
+                            two: "",
+                            three: "2个",
+                            four: "无限"
+                        },
+                    ]
+                },
+                {
+                    title:"用户管理",
+                    children:[
+                        {
+                            one: "外部用户身份",
+                            two: "多达50000个活跃用户",
+                            three: "多达50000个活跃用户",
+                            four: "根据需要"
+                        },
+                        {
+                            one: "外部用户身份",
+                            two: "多达50000个活跃用户",
+                            three: "多达50000个活跃用户",
+                            four: "根据需要"
+                        },
+                        {
+                            one: "外部用户身份",
+                            two: "多达50000个活跃用户",
+                            three: "多达50000个活跃用户",
+                            four: "根据需要"
+                        },
+                        {
+                            one: "外部用户身份",
+                            two: "多达50000个活跃用户",
+                            three: "多达50000个活跃用户",
+                            four: "根据需要"
+                        },
+                    ]
+                },
+                {
+                    title:"安全",
+                    children:[
+                        {
+                            one: "外部用户身份",
+                            two: "多达50000个活跃用户",
+                            three: "多达50000个活跃用户",
+                            four: "根据需要"
+                        },
+                        {
+                            one: "外部用户身份",
+                            two: "多达50000个活跃用户",
+                            three: "多达50000个活跃用户",
+                            four: "根据需要"
+                        },
+                    ]
+                },
+                {
+                    title:"企业个性化",
+                    children:[
+                        {
+                            one: "登录框组件、域名和邮件定制",
+                            two: "",
+                            three: "",
+                            four: ""
+                        },
+                        {
+                            one: "可自定义的邮件模版",
+                            two: "",
+                            three: "",
+                            four: ""
+                        },
+                        {
+                            one: "私有云部署服务",
+                            two: "",
+                            three: "可加购（38,199 人民币/年）",
+                            four: "已包括"
+                        },
+                        {
+                            one: "企业系统集成",
+                            two: "",
+                            three: "",
+                            four: ""
+                        },
+                        {
+                            one: "定制开发工程师",
+                            two: "",
+                            three: "799 RMB/人时",
+                            four: "高级版的 8 折"
+                        },
+                        {
+                            one: "单点登录应用面板",
+                            two: "",
+                            three: "",
+                            four: "根据需要"
+                        },
+                    ]
+                },
+                {
+                    title:"企业支持服务",
+                    children:[
+                        {
+                            one: "客服时间",
+                            two: "工作时间",
+                            three: "工作时间",
+                            four: "全年 24 小时"
+                        },
+                        {
+                            one: "响应时间",
+                            two: "24 小时内可紧急响应",
+                            three: "1 小时内可紧急响应",
+                            four: "30 分钟内可紧急响应"
+                        },
+                        {
+                            one: "微信客服",
+                            two: "",
+                            three: "",
+                            four: ""
+                        },
+                        {
+                            one: "服务级别",
+                            two: "",
+                            three: "99.9%",
+                            four: "99.9%"
+                        },
+                        {
+                            one: "应用对接支持、培训",
+                            two: "",
+                            three: "699 RMB/人时",
+                            four: "高级版的 8 折"
+                        },
+                        {
+                            one: "电话/会议客服",
+                            two: "",
+                            three: "",
+                            four: ""
+                        },
+                        {
+                            one: "专属客户经理",
+                            two: "",
+                            three: "",
+                            four: ""
+                        },
+                    ]
+                },
+            ]
+        }
+    }
+};
 </script>
 
 <style lang="scss" scoped>
 .detailNav {
   .price-message {
-    background: chartreuse;
     .inner {
       width: 1200px;
       margin: 0 auto;
@@ -55,6 +250,95 @@ export default {};
         }
       }
       .button-warp {
+        .authing-button {
+          border: 0;
+          background-color: transparent;
+          outline: none;
+          font-size: 16px;
+          width: 200px;
+          height: 50px;
+          background: #396aff;
+          display: flex;
+          align-items: center;
+          justify-content: center;
+          color: #fff;
+          cursor: pointer;
+          margin: 0 auto;
+          border-radius: 4px;
+          transition: All 0.2s ease-in-out;
+        }
+        .authing-button:hover {
+          background-color: #557fff;
+          transform: translateY(-2px);
+        }
+      }
+      .authing-title-wrapper {
+        margin-top: 70px;
+        p {
+          text-align: center;
+          color: #181818;
+          font-size: 36px;
+          margin-bottom: 10px;
+          line-height: 50px;
+          margin: 40px 0;
+        }
+        .table-title {
+          display: flex;
+          justify-content: flex-end;
+          margin-bottom: 30px;
+          color: #666;
+          span {
+            display: inline-block;
+            width: 270px;
+            text-align: center;
+            i {
+              color: #999;
+              margin-right: 6px;
+            }
+          }
+        }
+      }
+    }
+  }
+  .price-detail {
+    .table-content {
+      box-shadow: 0 0 40px 0 #f2f2f2;
+      width: 1100px;
+      margin: 0 auto;
+      .authing-table {
+        margin: 0 auto;
+        border-collapse: separate;
+        border-spacing: 0;
+        width: 1100px;
+        .tr {
+          height: 58px;
+          line-height: 58px;
+          background: #2c303e;
+          color: #fff;
+          padding: 0 30px;
+          text-align: left;
+          span {
+          }
+        }
+        .tbody {
+          height: 48px;
+          line-height: 48px;
+          color: #666;
+          font-size: 14px;
+          display: flex;
+          justify-content: flex-end;
+          p {
+            width: 250px;
+            text-align: center;
+          }
+          p:first-child {
+            width: 300px;
+            text-align: left;
+          }
+        }
+        .tbody:nth-child(2) ,.tbody:nth-child(4) ,.tbody:nth-child(6) ,.tbody:nth-child(8){
+            background: #f7f7f7;
+        }
       }
     }
   }
---
+++ src/components/price/QuestionNav.vue
@@ -0,0 +1,123 @@
+<template>
+  <div class="questionNav">
+    <div class="authing-title-wrapper">
+      <p class="title">常见问题</p>
+      <p class="description">
+        我们在这里解释你可能想知道的所有内容。让我们帮忙！
+      </p>
+    </div>
+    <div class="question-list">
+      <div class="item" v-for="(item, index) in list" :key="index">
+        <div class="left">
+          <i class="iconfont icon-liaotian"></i>
+        </div>
+        <div class="right">
+          <div class="question-title">{{ item.title }}</div>
+          <div class="question-content">
+            {{ item.contentOne }}
+            <ul></ul>
+            <router-link to="">{{ item.contentTwo }}</router-link>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      list: [
+        {
+          title: "我该如何选择 Authing 的技术？",
+          contentOne:
+            "我该如何选择 Authing 的技术？请根据你的具体情况选择需要使用的技术。 开始之前，我们建议你阅读此文：",
+          contentTwo: "为什么所有软件都应该使用单点登录来管理用户？",
+        },
+        {
+          title: "如何选择认证方式？",
+          contentOne:
+            "若你不理解什么是 OAuth、OIDC、SAML、LDAP 等标准协议且应用安全性要求不高，建议使用普通认证； 若你的应用安全性要求较高，无论是否了解标准协议，请使用标准协议认证。",
+          //   contentTwo: "为什么所有软件都应该使用单点登录来管理用户？",
+        },
+        {
+          title: "Authing 是什么？",
+          contentOne:
+            "Authing 提供专业的身份认证和授权服务。 我们为开发者和企业提供用以保证应用程序安全所需的认证模块，这让开发人员无需成为安全专家。 你可以将任意平台的应用接入到 Authing（无论是新开发的应用还是老应用都可以），同时你还可以自定义应用程序的登录方式（如：邮箱/密码、短信/验证码、扫码登录等）。 你可以根据你使用的技术，来选择我们的 SDK 或调用相关 API 来接入你的应用。当用户发起授权请求时，Authing 会帮助你认证他们的身份和返回必要的用户信息到你的应用中。",
+          //   contentTwo: "为什么所有软件都应该使用单点登录来管理用户？",
+        },
+        {
+          title: "为什么要使用 Authing？",
+          contentOne:
+            "下面是我们整理的 Authing 的部分使用场景:你想开发一个很酷的程序，这时你想添加用户身份认证和授权。你希望你的用户能使用微信、Github 登录，同时你还希望能追踪到用户的注册来源，活动数据，以便你做后续的用户增长。你做了一个 API，同时你希望能使用 OAuth 2.0 协议保证 API 的安全。你做了很多应用程序，你希望这些应用程序的用户数据可以通过单点登录（Single Sign On）的方式互通。你做了一个 JavaScript 前端应用和一个移动端应用，你希望这两个客户端应用都可以安全的访问你的 API。",
+          contentTwo: "阅读文档",
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.questionNav {
+  margin: 70px 0 40px 0;
+  .authing-title-wrapper {
+    padding: 40px 0;
+    text-align: center;
+    .title {
+      color: #181818;
+      font-size: 36px;
+      margin-bottom: 10px;
+      line-height: 50px;
+    }
+    .description {
+      color: #999;
+      line-height: 20px;
+      font-size: 16px;
+      text-align: center;
+      line-height: 26px;
+      margin: 0 auto;
+      max-width: 1070px;
+    }
+  }
+  .question-list {
+    display: flex;
+    width: 1100px;
+    margin: 0 auto;
+    flex-wrap: wrap;
+    justify-content: space-between;
+    .item {
+      padding: 16px;
+      display: flex;
+      width: 535px;
+      box-sizing: border-box;
+      .left {
+        i {
+          color: #396aff;
+          font-size: 30px;
+          margin-right: 16px;
+        }
+      }
+      .right {
+        flex-grow: 1;
+        .question-title {
+          font-weight: 700;
+          color: #333;
+          line-height: 20px;
+          height: 20px;
+          margin-bottom: 10px;
+        }
+        .question-content {
+          color: #666;
+          font-size: 14px;
+          line-height: 28px;
+          a {
+            color: #396aff;
+          }
+        }
+      }
+    }
+  }
+}
+</style>
